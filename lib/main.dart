d36896d200020e2359f89c22f37d6167e2b01b5e
Asim-Zaheer <iasimzaheer@gmail.com>
2024-06-27 14:02:15 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return   MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+      primarySwatch:Colors.purple
+      ),
+      home: const RootPage(
+
+      ),
+    );
+  }
+}  
+ 
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('First App'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        ),
+    );
+  }
+}
